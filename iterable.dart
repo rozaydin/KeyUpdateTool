deb4b2e292f9f5a52cf4a49695b2c5223f4d2039
rozaydin <ridvan.ozaydin@siemens.com>
2019-03-29 16:28:48 +0300

    updated example

--- iterable.dart
+++ iterable.dart
@@ -1,12 +1,11 @@
 import 'dart:core';
 import "package:pointycastle/pointycastle.dart";
+import "package:pointycastle/export.dart";
 
 printMenu() {
   print("1. Update key");
   print("2. Exit");
 }
-
-
 
 void main() {
   const String version = "v0.1";
@@ -15,11 +14,23 @@ void main() {
   print("------------------------");
   printMenu();
 
+  try {
+    RSAKeyGeneratorParameters params =
+        new RSAKeyGeneratorParameters(new BigInt.from(19), 1024, 80);
+    // create rsa key pair
 
-//  new RSAKeyGeneratorParameters(publicExponent, bitStrength, certainty)
+    var rsapars =
+        new RSAKeyGeneratorParameters(BigInt.parse("65537"), 2048, 12);
+    var params = new ParametersWithRandom(rsapars, rnd);
 
+    var keyGenerator = new KeyGenerator("RSA");
+    keyGenerator.init(params);
 
-
-
-
+    AsymmetricKeyPair keypair = keyGenerator.generateKeyPair();
+    print(keypair.publicKey);
+    print(keypair.privateKey);
+  } catch (exc, stacktrace) {
+    print(exc);
+    print(stacktrace);
+  }
 }
